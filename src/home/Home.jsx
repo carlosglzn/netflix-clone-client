4828766e15e4b4a49770010ef253d3970225b3ff
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-08-18 16:06:16 -0500

    Navbar and featured

---
+++ src/home/Home.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+import Featured from '../components/featured/Featured'
+import Navbar from '../components/navbar/Navbar'
+import './home.scss'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Navbar />
+            <Featured type="movie"/>
+        </div>
+    )
+}
+
+export default Home
