73034f284c36dc85db92ef272b7bcc041213d600
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-08-18 19:46:02 -0500

    Watch Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './mysass.scss'
-import Home from './home/Home'
+import Home from './pages/home/Home'
+import Watch from './pages/watch/Watch';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Watch/>
     </div>
   );
 }
