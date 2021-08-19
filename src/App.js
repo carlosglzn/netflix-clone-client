a7682ce75e640d993d1a43ac3cb155ac781c9a64
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-08-18 20:12:28 -0500

    Register page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './mysass.scss'
 import Home from './pages/home/Home'
+import Register from './pages/register/Register';
 import Watch from './pages/watch/Watch';
 
 function App() {
   return (
     <div className="App">
-      <Watch/>
+      <Register/>
     </div>
   );
 }
